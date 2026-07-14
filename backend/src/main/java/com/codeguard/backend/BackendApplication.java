e3e73d80c3e5a6f0e8990cdebd7879a81ab31029
Souvik31-k <souvik31.slg@outlook.com>
2026-07-15 00:10:28 +0530

    Docker and PostgreSQL integration

--- backend/src/main/java/com/codeguard/backend/BackendApplication.java
+++ backend/src/main/java/com/codeguard/backend/BackendApplication.java
@@ -3,10 +3,13 @@ package com.codeguard.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
